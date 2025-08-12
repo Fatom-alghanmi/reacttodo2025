b9dae9546f287950a3502fa0e45ff2ae813889be
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-08-12 09:15:44 -0400

    added checkedbox taggle

--- todo/src/TodoRow.js
+++ todo/src/TodoRow.js
@@ -1,26 +1,26 @@
 import React from 'react';
- 
+
 function TodoRow({item, toggle})
 {
- 
+
     const onToggle = () => {
         toggle(item);
     };
- 
+
     return (
-<tr>
-<td>
+        <tr>
+            <td>
                 { item.action }
-</td>
-<td>
-<input
+            </td>
+            <td>
+                <input
                     type="checkbox"
                     checked={ item.done }
                     onChange={ onToggle }
                     />
-</td>
-</tr>
+            </td>
+        </tr>
     );
 }
- 
+
 export default TodoRow;
