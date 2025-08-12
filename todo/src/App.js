219d5af1c8d37aef1d7fb08322a8fd3406396e0a
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-08-12 08:03:58 -0400

    added TodoBanner.js and TodoRow.js and codes

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState } from 'react';
+import TodoBanner from './TodoBanner';
+import TodoRow from './TodoRow';
 import './App.css';
 
 function App() {
+  const [userName] = useState("Fatom");
+
+  const [todoItems] = useState([
+    { action: "Buy Flowers", done: false },
+    { action: "Get Shoes", done: false },
+    { action: "Collect Tickets", done: true },
+    { action: "Call Joe", done: false }
+  ]);
+
+  //const changeStateData = () => {
+   // setUserName(prevName => (prevName === "Fatom" ? "Fuad" : "Fatom"));
+  //};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-3">
+      <TodoBanner userName={userName} todoItems={todoItems} />
+
+      <table className="table table-striped table-bordered">
+        <thead className="table-dark">
+          <tr>
+            <th>Action</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todoItems.map((item, index) => (            
+            <TodoRow item={item} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
